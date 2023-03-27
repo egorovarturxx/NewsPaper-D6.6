d132e4d6dae1350cc63de93a5d2cb64f669cdd1a
egorovarturxx <122422490+egorovarturxx@users.noreply.github.com>
2023-03-27 17:29:00 +0800

    Add files via upload

---
+++ NewsPaper/NewsPaper/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class NewsList(ListView):
+    model = Post
+    ordering = '-date'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news_quantity'] = len(Post.objects.all())
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
